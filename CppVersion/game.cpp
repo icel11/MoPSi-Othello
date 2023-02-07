b95752ddec9784f91bb8f0fc45c829689eaef992
Icel <icel.vinals@gmail.com>
2023-02-07 16:59:12 +0100

    Applying alpha beta pruning

--- CppVersion/game.cpp
+++ CppVersion/game.cpp
@@ -13,6 +13,7 @@ Game::Game(int aiColor)
     chips[3][4] = 1;
     chips[4][3] = 1;
     chips[4][4] = -1;
+
     gamePosition = Position(chips, turn);
     this->aiColor = aiColor;
     draw();
@@ -23,16 +24,26 @@ void Game::changeTurn() {
     gamePosition.setAvailable(turn);
 }
 
+int Game::calculateScore(Position position, int color) {
+    int score = 0;
+    for(int row = 0; row < 8; row++) {
+        for(int col = 0; col < 8; col++){
+            score += weightMatrix[row][col] * position.get(row, col);
+        }
+    }
+    return score * (-1);
+}
+
 
 int calculateScore(Position position, int color) {
     int score = 0;
     for(int row = 0; row < 8; row++) {
         for(int col = 0; col < 8; col++){
             if(position.get(row, col) == 1) {
-                score += abs(row-3.5)+abs(col-3.5);
+                score += pow(abs(row-3.5)+abs(col-3.5), 2);
             }
             if(position.get(row, col) == -1) {
-                score -= abs(row-3.5)+abs(col-3.5);
+                score -= pow(abs(row-3.5)+abs(col-3.5), 2);
             }
         }
     }
@@ -43,30 +54,50 @@ int calculateScore(Position position, int color) {
 }
 
 
-pair<Position, int> Game::calculateMovement(Position position, int color, int depth) {
-    if(depth == 0)
-        return make_pair(position, calculateScore(position, color));
-    else {
-        vector<pair<Position, int>> possibleMouvements;
+pair<Position, int> Game::alfabeta(Position position, int depth, int alfa, int beta, int turn, bool maximizing_player) {
+    if(depth == 0 || position.noMove())
+        return make_pair(position, this->calculateScore(position, turn));
+    if(maximizing_player) {
+        pair<Position, int> value = make_pair(position, -INT_MAX);
         for(int row = 0; row < 8; row++) {
             for(int col = 0; col < 8; col++){
                 if(position.get(row, col) == 2) {
                     Position newPos(position);
-                    int auxColor = color;
-                    newPos.addChip(row, col, auxColor);
-                    possibleMouvements.push_back(make_pair(newPos, calculateMovement(newPos, auxColor, depth - 1).second));
+                    int auxTurn = turn;
+                    newPos.addChip(row, col, auxTurn);
+                    int new_value = alfabeta(newPos, depth-1, alfa, beta, auxTurn, false).second;
+                    if(new_value > value.second) {
+                        value = make_pair(newPos, new_value);
+                    }
+                    if(value.second > beta) {
+                        break;
+                    }
+                    alfa = max(alfa, value.second);
                 }
             }
         }
-        if(possibleMouvements.empty()) {
-            return make_pair(position, calculateScore(position, color));
+        return value;
+    }
+    else {
+        pair<Position, int> value = make_pair(position, INT_MAX);
+        for(int row = 0; row < 8; row++) {
+            for(int col = 0; col < 8; col++){
+                if(position.get(row, col) == 2) {
+                    Position newPos(position);
+                    int auxTurn = turn;
+                    newPos.addChip(row, col, auxTurn);
+                    int new_value = alfabeta(newPos, depth-1, alfa, beta, auxTurn, true).second;
+                    if(new_value < value.second) {
+                        value = make_pair(newPos, new_value);
+                    }
+                    if(value.second < alfa) {
+                        break;
+                    }
+                    beta = min(beta, value.second);
+                }
+            }
         }
-        pair<Position, int> bestMouvement = make_pair(Position(), INT_MIN);
-        for(pair<Position, int> pos : possibleMouvements) {
-            if(pos.second > bestMouvement.second)
-                bestMouvement = pos;
-        }
-        return bestMouvement;
+        return value;
     }
 }
 
@@ -86,7 +117,7 @@ Position Game::getHumanMovement() {
 
 void Game::update() {
     if(turn == aiColor) {
-        gamePosition = calculateMovement(gamePosition, turn, 4).first;
+        gamePosition = alfabeta(gamePosition, 3, -INT_MAX, INT_MAX, turn, true).first;
     } else {
         gamePosition = getHumanMovement();
     }
--- CppVersion/game.h
+++ CppVersion/game.h
@@ -7,11 +7,21 @@ class Game
 private:
     Display display = Display();
     Position gamePosition;
-    pair<Position, int> calculateMovement(Position position, int color, int depth);
+    int calculateScore(Position position, int color);
+    pair<Position, int> alfabeta(Position position, int depth, int alfa, int beta, int turn, bool maximizing_player);
     Position getHumanMovement();
     bool finished = false;
     int aiColor = 0;
     int turn = 1;
+    int weightMatrix[8][8] = {{100,-25, 10,  5,  5, 10,-25, 100},
+                              {-25,-25,  2,  2,  2,  2,-25, -25},
+                              { 10,  2,  5,  1,  1,  5,  2,  10},
+                              {  5,  2,  1,  2,  2,  1,  2,   5},
+                              {  5,  2,  1,  2,  2,  1,  2,   5},
+                              { 10,  2,  5,  1,  1,  5,  2,  10},
+                              {-25,-25,  2,  2,  2,  2,-25, -25},
+                              {100,-25, 10,  5,  5, 10,-25, 100}};
+
     void changeTurn();
 public:
     Game(int aiColor);
